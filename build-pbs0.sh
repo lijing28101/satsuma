bb9ec7d92dde48d2e7c44dcaefdc4be2230ebc7e
jingli <jingli@iastate.edu>
2016-10-26 10:20:33 -0500

    Miscellaneous changes

--- build-pbs0.sh
+++ build-pbs0.sh
@@ -2,10 +2,10 @@
 set -u
 
 # Glycine max file (e.g. xxx1110.fasta)
-target=$1
+target=Glycine_max
 # Other genome, e.g. Glycine_soja
-query=$2
+query=$1
 
-cat satsuma-single.pbs |
+cat satsuma0.pbs |
     sed "s/QUERY/$query/" |
     sed "s/TARGET/$target/" > $query.vs.$target.pbs
